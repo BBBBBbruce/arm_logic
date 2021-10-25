d012a02687ee10fa5e374cb31f68609bd32e244a
Bruce Zhang <s2237238@ed.ac.uk>
2021-10-25 16:18:49 +0100

    init interface class

---
+++ interface.h
@@ -0,0 +1,17 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include"arm.h"
+#include "gamepad.h"
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+
+#endif
