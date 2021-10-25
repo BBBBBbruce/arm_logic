f5bf5fd81a8d58eb6b67c419bc8c73dfb9bf47ce
BBBBBbruce <bubblebruce@outlook.com>
2021-10-25 12:30:31 +0100

    add vulkan for graphical interface

--- arm_logic.cpp
+++ arm_logic.cpp
@@ -1,8 +1,13 @@
 //rotation reference always x-axis
+#define GLFW_INCLUDE_VULKAN
 #include"arm.h"
 #include "gamepad.h"
+#include <GLFW/glfw3.h>
 
-
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 
 int main()
